3dc105bb283a4f402b6dbe522b44b41173f51d57
Jason Wang <jason.wang@outlook.com>
2016-05-19 12:19:03 -0700

    Added/fixed docstrings

--- satspot.py
+++ satspot.py
@@ -1,8 +1,10 @@
 def minimize_psf(p, im1, im2, return_res_arr = False):
-	# Function used by op.minimize to fit PSF
-	# p[0] = xoffset applied to im2
-	# p[1] = yoffset applied to im2
-	# p[2] = flux ratio between im1 and im2 (i.e. value to multiply im2 by to match im1)
+	"""
+	Function used by op.minimize to fit PSF
+	p[0] = xoffset applied to im2
+	p[1] = yoffset applied to im2
+	p[2] = flux ratio between im1 and im2 (i.e. value to multiply im2 by to match im1)
+	"""
 
 	if np.shape(im1) != np.shape(im2):
 		print 'Mismatching dimensions'
@@ -30,16 +32,17 @@ def minimize_psf(p, im1, im2, return_res_arr = False):
 		return res
 
 def psf_fit(im1, im2, p0, return_res_arr=False):
-
-	#Inputs:
-	#	im1 - first image
-	#	im2 - second image, same dimensions
-	#	p0 - vector of guesses for PSF fit [xoffset, yoffset, flux_ratio]
-	#Outputs:
-	#	p - vector of PSF fitting results [xoffset, yoffset, flux_ratio]
+	"""
+	Args:
+		im1 - first image
+		im2 - second image, same dimensions
+		p0 - vector of guesses for PSF fit [xoffset, yoffset, flux_ratio]
+	Return:
+		p - vector of PSF fitting results [xoffset, yoffset, flux_ratio]
 	
-	# Function to peform PSF fitting. Return residual array if true.
-	# Using scipy.optimize.minimize to do this
+	 Function to peform PSF fitting. Return residual array if true.
+	 Using scipy.optimize.minimize to do this
+	"""
 
 	#I've set up some conservative limits on the fitting,
 	# the x and y offset should be less than a pixel, and the flux ratio should be within a factor of a hundred
@@ -53,30 +56,32 @@ def psf_fit(im1, im2, p0, return_res_arr=False):
 		return p
 
 def fitxy (im, xy_guess):
+	""""
+	Args:
+		im - 2D image
+	   xy_guess - initial guess of source to fit
+	Return:
+		xy - Tuple of fitted xy coordinates
 
-	#Inputs:
-	#	im - 2D image
-	#   xy_guess - initial guess of source to fit
-	#Outputs:
-	#	xy - Tuple of fitted xy coordinates
-
-	# Function which fits the location of a point source within an image
-	# Same as return_pos in gpi_satspotcalib_wd.py
-
+	 Function which fits the location of a point source within an image
+	 Same as return_pos in gpi_satspotcalib_wd.py
+	"""
+	
 	return xy
 
 def stamp(im, xy, sout):
+	"""
+	Args:
+	   im - 2D image of arbitrary dimensions (281x281 for GPI)
+	   xy - 2tuple containing x,y coordinates of spot/point source
+		sout - size of output image, with source shifted to (sout/2, sout/2)
+	Return:
+		stamp - 2d image of size (sout, sout)
 
-	#Inputs:
-	#   im - 2D image of arbitrary dimensions (281x281 for GPI)
-	#   xy - 2tuple containing x,y coordinates of spot/point source
-	#	sout - size of output image, with source shifted to (sout/2, sout/2)
-	#Output:
-	#	stamp - 2d image of size (sout, sout)
-
-	# Use ndimage.map_coordinates to create the stamp by interpolation.
-	# See lines 365-374 in gpi_satspotcalib_wd.py
-
+	 Use ndimage.map_coordinates to create the stamp by interpolation.
+	 See lines 365-374 in gpi_satspotcalib_wd.py
+	"""
+	
 	return stamp
 
 
